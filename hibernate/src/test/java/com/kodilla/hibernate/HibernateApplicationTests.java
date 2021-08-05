80cfb3b3a1af99f039431ae344d9e2a7a962c2d8
Krystian Piotrowski <piotrowski24h@interia.pl>
2021-08-05 16:33:25 +0200

    hibernate-start

---
+++ hibernate/src/test/java/com/kodilla/hibernate/HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.hibernate;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ kodilla-hi/src/main/java/com/kodilla/hi/KodillaHiApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.hi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHiApplication.class, args);
+    }
+
+}
